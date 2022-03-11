e0fdfe4688565c6cd6a67aae96e161770db8b82e
Eliel <elielmichel@gmail.com>
2022-03-11 16:26:54 -0300

    build: configure project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+
+import { theme } from '@/src/styles/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
